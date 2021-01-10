09007d0b9e16467c6a901314c600b2146577d0cb
Talhaonge058 <76546550+Talhaonge058@users.noreply.github.com>
2021-01-10 08:18:29 +0300

    Add files via upload

---
+++ sunucuAyar.js
@@ -0,0 +1,13 @@
+const ayarlar = {
+  guildID: "793304412294611056", // sunucu idsi
+  ownerRole:"793350747756429373", // sunucu sahiplerinin rolü
+  teyitsizRolleri:[""], // kayıt olmayanlara verieln rol
+  jailRolu:"", // cezalılara verilen rol
+  enAltYetkiliRolu:[''], // ilk yetkili permi
+  activity:"⛧ Quantum#COMEBACK", // botun durumu
+  status:"online",
+  invitelink:"https://discord.gg/qVxZDjNkKT", //davet linki
+  symbol:"⛧", // tag
+  voicechannel:"795593308273246218" // botun gireceği ses kanalı
+};
+module.exports = ayarlar;
